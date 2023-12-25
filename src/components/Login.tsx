b00c0f13ab6a0c0e6112f39c9df03c87f279a6f0
Meryem İris <irismeryem.96@gmail.com>
2023-12-25 21:50:12 +0300

    add supabase

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,21 +1,72 @@
+import { supabase } from "@/lib/supabase";
 import styles from "./Login.module.css";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Login() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.signInWithPassword({
+        email: username,
+        password,
+      });
+
+      if (error) {
+        console.error("Error signing in:", error.message);
+      } else {
+        console.log("User signed in successfully:", user);
+
+        router.push("/");
+      }
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
+  };
+
   return (
-    <form className={styles.login}>
+    <form
+      className={styles.login}
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleLogin();
+      }}
+    >
       <label className={styles.label} htmlFor="username">
         username
       </label>
-      <input className={styles.input} type="text" id="username" />
+      <input
+        className={styles.input}
+        name="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="username"
+        type="text"
+        id="username"
+      />
       <label className={styles.label} htmlFor="password">
         password
       </label>
-      <input className={styles.input} type="password" id="password" />
-      <button className={styles.button}>Login</button>
+      <input
+        className={styles.input}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        id="password"
+      />
+      <button className={styles.button} type="submit">
+        Login
+      </button>
 
       <p className={styles.text}>
         Do not have an account?{" "}
-        <a className={styles.link} href="/signup">
+        <a className={styles.link} href="signup">
           Signup
         </a>
       </p>
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,26 +1,74 @@
+import { useState } from "react";
+import { supabase } from "../lib/supabase";
+
 import styles from "./Login.module.css";
+import { useRouter } from "next/router";
 
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const router = useRouter();
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.signUp({
+        email: username,
+        password,
+      });
+
+      if (!error) {
+        console.log("User registered successfully:", user);
+        router.push("/login");
+      }
+    } catch (error) {
+      console.error("Error registering user:", error);
+    }
+  };
   return (
-    <form className={styles.login}>
+    <form onSubmit={handleRegister} className={styles.login}>
       <label className={styles.label} htmlFor="email">
         email
       </label>
-      <input className={styles.input} type="email" id="email" />
+      <input
+        className={styles.input}
+        type="email"
+        id="email"
+        onChange={(e) => setEmail(e.currentTarget.value)}
+        required
+      />
 
       <label className={styles.label} htmlFor="username">
         username
       </label>
-      <input className={styles.input} type="text" id="username" />
+      <input
+        className={styles.input}
+        type="text"
+        id="username"
+        onChange={(e) => setUsername(e.currentTarget.value)}
+        required
+      />
       <label className={styles.label} htmlFor="password">
         password
       </label>
-      <input className={styles.input} type="password" id="password" />
+      <input
+        className={styles.input}
+        type="password"
+        id="password"
+        onChange={(e) => setPassword(e.currentTarget.value)}
+        required
+      />
       <button className={styles.button}>Signup</button>
 
       <p className={styles.text}>
         Already have an account?
-        <a className={styles.link} href="/login">
+        <a className={styles.link} href="login">
           Login
         </a>
       </p>

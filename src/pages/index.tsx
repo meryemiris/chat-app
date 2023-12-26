b7aa0ef742f0eaee59d2bc620501a64c8db6d502
Meryem İris <irismeryem.96@gmail.com>
2023-12-26 19:51:56 +0300

    subscribe supabase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,70 @@
+import { supabase } from "@/lib/supabase";
 import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
 import { IoSend } from "react-icons/io5";
 
+type Message = {
+  sender_username: string;
+  content: string;
+  id: number;
+  created_at: string;
+};
+
 export default function Home() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("messages")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "messages" },
+        (payload: { new: Message }) => {
+          console.log(payload);
+          setMessages((prev) => [...prev, payload.new]);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, []);
+
+  const handleSendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const message = formData.get("message");
+
+    if (!message) return;
+
+    try {
+      const { data, error } = await supabase
+        .from("messages")
+        .insert([{ sender_username: "", content: message }])
+        .select();
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log("subscribe");
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.chatBox}>
-        <div className={styles.messageBox}>
-          <p className={styles.username}>me</p>
-          <p className={styles.content}>Hi, what are you doing?</p>
-        </div>
-        <div className={styles.messageBox}>
-          <p className={styles.username}>you</p>
-          <p className={styles.content}>
-            I am coding, what about you? Are you gonna go to the gym today?
-          </p>
-        </div>
+        {messages.map(({ sender_username, content, id, created_at }) => (
+          <div key={id} className={styles.messageBox}>
+            <p className={styles.username}>{sender_username}</p>
+            <p className={styles.content}>{content}</p>
+            <p className="text-xs text-gray-400">{created_at}</p>
+          </div>
+        ))}
       </div>
 
-      <form className={styles.sendBox}>
+      <form onSubmit={handleSendMessage} className={styles.sendBox}>
         <input type="text" placeholder="Type something..." name="message" />
-        <button>
+        <button type="submit">
           <IoSend />
         </button>
       </form>

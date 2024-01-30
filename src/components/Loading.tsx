10879d6b64942b25c809c82e12e93222b2acbf97
Meryem İris <irismeryem.96@gmail.com>
2024-01-30 21:47:52 +0300

    add loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+import styles from "./Loading.module.css";
+
+export default function Loading() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+      <div className={styles.shadow}></div>
+      <div className={styles.shadow}></div>
+      <div className={styles.shadow}></div>
+    </div>
+  );
+}
